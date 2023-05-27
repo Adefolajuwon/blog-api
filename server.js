06d1eac61eed0fedcb1cf778b558eb5c163287fe
adefolajuwon oyebola <oadefolajuwon@gmail.com>
2023-05-27 13:34:33 +0100

    change

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const http = require('http');
 const app = require('.');
-
+require('dotenv').config();
+const PORT = process.env.SERVER;
 const { startMongoose } = require('./lib/mongoose');
 
-const PORT = 8080;
+const Server = 8080 || PORT;
 const server = http.createServer(app);
 
 (async function () {
 	server.listen(PORT, () => {
-		console.log(`Server started on PORT ${PORT}.....`);
+		console.log(`Server started on PORT ${Server}.....`);
 	});
 	await startMongoose();
 })();
